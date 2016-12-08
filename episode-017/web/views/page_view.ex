b120f4038a01d802f2a0dae559290b1dbd9a52cb
Erik Person <erik@thaterikperson.com>
2016-12-08 13:38:09 -0500

    Episode 017

---
+++ episode-017/web/views/page_view.ex
@@ -0,0 +1,12 @@
+defmodule ElmIsFun.PageView do
+  use ElmIsFun.Web, :view
+
+  def render("create.json", assigns) do
+    %{
+      status: "ok",
+      number_of_hands: assigns[:hands] || 0,
+      number_of_wins: 0,
+      favorite_casino: nil,
+    }
+  end
+end
